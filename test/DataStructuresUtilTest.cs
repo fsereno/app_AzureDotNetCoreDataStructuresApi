6713ec08b53989ed0a6b6366d3b088fa5e5cb6ed
Fabio Sereno <fabio_sereno@hotmail.com>
2020-12-16 23:01:20 +0000

    Working out structure

--- test/DataStructuresUtilTest.cs
+++ test/DataStructuresUtilTest.cs
@@ -1,24 +1,62 @@
-using System;
 using System.Collections;
 using Xunit;
 
-namespace Test
+namespace FS.Test.DataStructuresUtilTest
 {
+    public interface ICollectionHandler<T>
+    {
+        void Add(T collection, string value);
+        void Remove(T collection);
+    }
+
+    public class StackHandler<T> : ICollectionHandler<T> where T : Stack
+    {
+        public void Add(T collection, string value)
+        {
+            collection.Push(value);
+        }
+
+        public void Remove(T collection)
+        {
+            collection.Pop();
+        }
+    }
+
+    public class QueueHandler<T> : ICollectionHandler<T> where T : Queue
+    {
+        public void Add(T collection, string value)
+        {
+            collection.Enqueue(value);
+        }
+
+        public void Remove(T collection)
+        {
+            collection.Dequeue();
+        }
+    }
+
     public class DataStructuresUtilTest
     {
         private readonly Queue _queue;
         private readonly Stack _stack;
 
+        private readonly ICollectionHandler<Queue> _queueHandler;
+
+        private readonly ICollectionHandler<Stack> _stackHandler;
+
         public DataStructuresUtilTest()
         {
             _queue = new Queue();
             _stack = new Stack();
+
+            _queueHandler = new QueueHandler<Queue>();
+            _stackHandler = new StackHandler<Stack>();
         }
 
         [Fact]
         public void AddShouldAddItemToQueue()
         {
-            _queue.Enqueue("Item 1");
+            _queueHandler.Add(_queue, "Item 1");
 
             var result = _queue.Count;
             Assert.Equal(1, result);
@@ -27,8 +65,8 @@ namespace Test
         [Fact]
         public void RemoveShouldRemoveItemFromQueue()
         {
-            _queue.Enqueue("Item 1");
-            _queue.Dequeue();
+            _queueHandler.Add(_queue, "Item 1");
+            _queueHandler.Remove(_queue);
 
             var result = _queue.Count;
             Assert.Equal(0, result);
@@ -37,7 +75,7 @@ namespace Test
         [Fact]
         public void AddShouldAddItemToStack()
         {
-            _stack.Push("Item 1");
+            _stackHandler.Add(_stack, "Item 1");
 
             var result = _stack.Count;
             Assert.Equal(1, result);
@@ -46,8 +84,8 @@ namespace Test
         [Fact]
         public void RemoveShouldRemoveItemFromStack()
         {
-             _stack.Push("Item 1");
-             _stack.Pop();
+            _stackHandler.Add(_stack, "Item 1");
+            _stackHandler.Remove(_stack);
 
             var result = _stack.Count;
             Assert.Equal(0, result);
