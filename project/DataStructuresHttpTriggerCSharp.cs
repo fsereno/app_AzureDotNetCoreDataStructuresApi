16cd099cf3857fbee087dfe921890a42c8b71881
Fabio Sereno <fabio_sereno@hotmail.com>
2020-12-28 22:11:43 +0000

    Working on naming and tidy up

--- project/DataStructuresHttpTriggerCSharp.cs
+++ project/DataStructuresHttpTriggerCSharp.cs
@@ -24,38 +24,11 @@ namespace FabioSereno.App_AzureDotNetCoreDataStructuresApi.Azure.Function
             _stackHandler = stackHandler;
         }
 
-        [FunctionName("DataStructuresHttpTriggerCSharp")]
-        public async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+        [FunctionName("AddQueueItemAsync")]
+        public async Task<IActionResult> AddQueueItemAsync([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
-
-            string name = req.Query["name"];
-
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-            var array = new string[] { "1", "2", "3"};
-            var queue = new Queue(array);
-            var stack = new Stack(array);
-
-            _queueHandler.Add(queue, "4");
-            _stackHandler.Add(stack, "4");
-
-            return new OkObjectResult(responseMessage);
-        }
-
-        [FunctionName("AddQueueItem")]
-        public async Task<IActionResult> AddQueueItem([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
-            ILogger log)
-        {
-            log.LogInformation("AddQueueItem endpoint hit");
+            log.LogInformation("AddQueueItemAsync endpoint hit");
 
             AddRequestBody data;
             string result = string.Empty;
@@ -81,11 +54,11 @@ namespace FabioSereno.App_AzureDotNetCoreDataStructuresApi.Azure.Function
             return new OkObjectResult(result);
         }
 
-        [FunctionName("RemoveQueueItem")]
-        public async Task<IActionResult> RemoveQueueItem([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+        [FunctionName("RemoveQueueItemAsync")]
+        public async Task<IActionResult> RemoveQueueItemAsync([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("RemoveQueueItem endpoint hit");
+            log.LogInformation("RemoveQueueItemAsync endpoint hit");
 
             AddRequestBody data;
             string result = string.Empty;
@@ -111,11 +84,11 @@ namespace FabioSereno.App_AzureDotNetCoreDataStructuresApi.Azure.Function
             return new OkObjectResult(result);
         }
 
-        [FunctionName("AddStackItem")]
-        public async Task<IActionResult> AddStackItem([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+        [FunctionName("AddStackItemAsync")]
+        public async Task<IActionResult> AddStackItemAsync([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("AddStackItem endpoint hit");
+            log.LogInformation("AddStackItemAsync endpoint hit");
 
             AddRequestBody data;
             string result = string.Empty;
@@ -141,11 +114,11 @@ namespace FabioSereno.App_AzureDotNetCoreDataStructuresApi.Azure.Function
             return new OkObjectResult(result);
         }
 
-        [FunctionName("RemoveStackItem")]
-        public async Task<IActionResult> RemoveStackItem([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+        [FunctionName("RemoveStackItemAsync")]
+        public async Task<IActionResult> RemoveStackItemAsync([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("RemoveStackItem endpoint hit");
+            log.LogInformation("RemoveStackItemAsync endpoint hit");
 
             AddRequestBody data;
             string result = string.Empty;
